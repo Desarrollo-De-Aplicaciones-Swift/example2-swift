815a376e290064f702ec1c84a69af050d71150f0
Moisés Sánchez <moy.sav21@hotmail.com>
2022-09-03 00:39:06 -0500

    feat: :sparkles: header collection view with label

--- NavigationApp/FavoritesViewController.swift
+++ NavigationApp/FavoritesViewController.swift
@@ -16,13 +16,40 @@ class FavoritesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        favoritePosterImage.image = UIImage(named: "0")
+        
+        //favoritePosterImage.image = UIImage(named: "0")
+        
+        posterPageControl.numberOfPages = posters.count
+        favoritePosterImage.image = UIImage(named: posters.first!)
     }
 
     @IBAction func posterPageControlAction(_ sender: UIPageControl) {
         
         favoritePosterImage.image = UIImage(named: posters[posterPageControl.currentPage])
+        
+    }
+    
+    
+    @IBAction func RightSwipe(_ sender: Any) {
+        print("Right Swipe")
+        posterPageControl.currentPage = posterPageControl.currentPage + 1
+               favoritePosterImage.image = UIImage(named: posters.first!)
+        
+    }
+    
+    @IBAction func leftRight(_ sender: Any) {
+        print("Left Swipe")
+        if posterPageControl.currentPage == 0 {
+            posterPageControl.currentPage = posters.count - 1
+            favoritePosterImage.image = UIImage(named: posters[posterPageControl.currentPage])
+            
+        }
+        else {
+            posterPageControl.currentPage = posterPageControl.currentPage - 1
+            favoritePosterImage.image = UIImage(named: posters[posterPageControl.currentPage])
+            
+        }
+        
         
     }
 }
